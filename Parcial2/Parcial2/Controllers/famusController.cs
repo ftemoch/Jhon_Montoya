5f4b43612a8ade834c63275ba7f981588891571f
jtmon <jtmon@DESKTOP-397MRLL>
2018-10-12 10:35:55 -0500

    trabajo realizado enviado minutos tarde porque se me bloqueo el computador

---
+++ Parcial2/Parcial2/Controllers/famusController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Parcial2.Models;
+
+
+
+namespace Parcial2.Controllers
+{
+    public class famusController : Controller
+    {
+        // GET: famus
+        public ActionResult Ramdon()
+        {
+            var cliente = new cliente()
+            {
+                Nombre = "jhon ",
+                Apellido = "Montoya",
+                Sueldo = 12000000
+            };
+            return View(muesta);
+            
+
+    }
+    }
+}
---
+++ Parcial2/Parcial2/Models/cliente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Parcial2.Models
+{
+    public class cliente
+    {
+        public int iD { get; set; }
+        public String Nombre { get; set; }
+        public String Apellido { get; set; }
+        public decimal Sueldo { get; set; }
+    }
+}
